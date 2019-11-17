b649515d3184e8d8f30466b92c02d930142b48b4
Wesley Pereira dos Santos <wesleyp_santos@outlook.com>
2019-11-16 22:50:12 -0300

    Create a model, view and controller

---
+++ MvcBasic/MvcBasic/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using MvcBasic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBasic.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var cliente = new Cliente()
+            {
+                Nome = "Asp",
+                SobreNome = "NET",
+                DataCadastro = DateTime.Now
+            };
+            return View(cliente);
+        }
+    }
+}
