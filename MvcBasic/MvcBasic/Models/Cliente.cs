f4212368f47fade884cb8613eb301dd184044997
Wesley Pereira dos Santos <wesleyp_santos@outlook.com>
2019-11-26 09:06:05 -0300

    Subject create a view using a template Problem There is not create a view Solution After the creation of data anotations in model of cliente, was create a view using the template in her creation. Note

--- MvcBasic/MvcBasic/Models/Cliente.cs
+++ MvcBasic/MvcBasic/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,30 @@ namespace MvcBasic.Models
 {
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+
+        [MaxLength(150, ErrorMessage = "Máximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Minimo de 2 caracteres")]
+        [Display(Name = "Nome do cliente")]
+        [Required(ErrorMessage = "Preencher campo nome")]
         public string Nome { get; set; }
+
+        [MaxLength(150, ErrorMessage = "Máximo de caracteres excedido")]
+        [MinLength(2, ErrorMessage = "Minimo de 2 caracteres")]
+        [Display(Name = "Sobrenome do cliente")]
+        [Required(ErrorMessage = "Preencher campo sobrenome")]
         public string SobreNome { get; set; }
+
+        [ScaffoldColumn(false)]
         public DateTime DataCadastro { get; set; }
+
+        [MaxLength(150, ErrorMessage = "Máximo de caracteres execedido")]
+        [MinLength(2, ErrorMessage = "Minimo de 2 caracteres")]
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage ="Preencher o campo E-mail")]
+        [EmailAddress(ErrorMessage = "E-mail preenchido no formato invalido")]
+        public string Email { get; set; }
 
     }
 }
